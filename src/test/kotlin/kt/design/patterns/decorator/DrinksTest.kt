e4a22ac4cfd62971037d0aa091200e234b2a31a3
imaginalis <rafbel001@utp.edu.pl>
2021-06-30 08:09:22 +0200

    Delete useless printout test logic from the decorator test

--- src/test/kotlin/kt/design/patterns/decorator/DrinksTest.kt
+++ src/test/kotlin/kt/design/patterns/decorator/DrinksTest.kt
@@ -1,25 +1,9 @@
 package kt.design.patterns.decorator
 
 import org.junit.Test
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.BeforeEach
-import java.io.ByteArrayOutputStream
-import java.io.PrintStream
 
 internal class DrinksTest {
-    private val standardOut = System.out
-    private val outputStreamCaptor = ByteArrayOutputStream()
-
-    @BeforeEach
-    fun setUp() {
-        System.setOut(PrintStream(outputStreamCaptor))
-    }
-
-    @AfterEach
-    fun tearDown() {
-        System.setOut(standardOut)
-    }
 
     @Test
     fun `should serve drinks with additives and return a valid cost`() {
